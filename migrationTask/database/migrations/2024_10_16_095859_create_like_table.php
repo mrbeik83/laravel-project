88f1f26508757873681bf2af4a9c91b74e18a696
mrbeik <rezabeikyazdi@gmail.com>
2024-10-16 14:17:33 +0330

    add migration

--- migrationTask/database/migrations/2024_10_16_095859_create_like_table.php
+++ migrationTask/database/migrations/2024_10_16_095859_create_like_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\table;
+
 return new class extends Migration
 {
     /**
@@ -11,14 +13,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('like', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('likedBy');
+            $table->string('productName');
+            $table->timestamp('likedAt')->nullable();
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('like');
     }
 };
