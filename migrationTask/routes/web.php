63886cd9ba388101f99b91d6ce03dad8f9cea195
mrbeik <rezabeikyazdi@gmail.com>
2025-05-12 02:51:21 +0330

    auth or login & register in laravel for helped breeze
    install npm and run install node.js

--- migrationTask/routes/web.php
+++ migrationTask/routes/web.php
@@ -1,43 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\productController;
-use App\Http\Controllers\OrderController;
-use App\Http\Middleware\EnsureTokenIsValid;
-use Illuminate\Support\Facades\App;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'user'], function () {
-    //نمایش تمام رکورد های یک جدول
-    Route::get('/search',[UserController::class,'fetchAll'])->name('user.serch');
-    //نمایش یک رکورد از یک جدول با استفاده از آی دی
-    Route::get('/search/id',[UserController::class,'fetchById']);
-     //نمایش لیست
-    Route::get('/list', [UserController::class, 'list'])->name('user.list');
-    //اضافه کردن یوزر
-    Route::get('/register',[UserController::class,'registerForm'])->name('register_form');
-    Route::post('/register/form_submit',[UserController::class,'insertUser'])->name('insertUser');
-    });
-    //
-    
-Route::group(['prefix' => 'product'] , function (){
-    Route::get('/register',[productController::class , 'create'])->name('product.create');
-    Route::post('/register',[productController::class ,'store'])->name('product.store');
-    Route::get('/list',[productController::class,'list'])->name('product.list');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-Route::group(['prefix' => 'order'],function(){
-    Route::get('/view',[OrderController::class,'viewCart']);
-    Route::post('/add',[OrderController::class,'addCart']);
-});
-
-
-
-
-
-
+require __DIR__.'/auth.php';
